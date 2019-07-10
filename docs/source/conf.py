0faa1d7a5223c760e0d3d7c95f0ae82fdbc35b4c
Shawn M. Jones <jones.shawn.m@gmail.com>
2019-07-10 13:35:35 -0600

    raising version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2019.06.17.193810'
+release = u'0.2019.07.10.193525'
 
 
 # -- General configuration ---------------------------------------------------
--- raiseversion.sh
+++ raiseversion.sh
@@ -3,9 +3,10 @@
 # Update version in project
 VERSION_STRING=`date -u +0.%Y.%m.%d.%H%M%S`
 FILE_NAME='raintale/version.py'
-# DOC_FILE_NAME='docs/source/conf.py'
+DOC_FILE_NAME='docs/source/conf.py'
 
-# Update mementoembed version
+# Update Raintale version
 sed -i.bak "s/^__appversion__ = .*$/__appversion__ = '$VERSION_STRING'/g" $FILE_NAME
 rm $FILE_NAME.bak
-# sed -i.bak "s/^release = '.*'$/release = '$VERSION_STRING'/g" $DOC_FILE_NAME
+sed -i.bak "s/^release = u'.*'$/release = u'$VERSION_STRING'/g" $DOC_FILE_NAME
+rm $DOC_FILE_NAME.bak
