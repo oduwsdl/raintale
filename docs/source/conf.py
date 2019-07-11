a1753ee1c5bf7211495b131f6b07a9d9c249593f
Shawn M. Jones <jones.shawn.m@gmail.com>
2019-07-11 08:28:12 -0600

    fix for mementos containing (non-animated) GIFs posting to Twitter

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2019.07.11.031608'
+release = u'0.2019.07.11.140435'
 
 
 # -- General configuration ---------------------------------------------------
--- raintale/version.py
+++ raintale/version.py
@@ -1,3 +1,3 @@
 __appname__ = "raintale"
-__appversion__ = '0.2019.07.11.031608'
+__appversion__ = '0.2019.07.11.140435'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
