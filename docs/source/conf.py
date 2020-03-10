cdf83b4ac0eabbc7f0abc404b4d671af47094eb7
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-03-09 19:39:11 -0600

    now generation_date is configurable and useable in templates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2020.03.03.002430'
+release = u'0.2020.03.10.013046'
 
 
 # -- General configuration ---------------------------------------------------
--- raintale/version.py
+++ raintale/version.py
@@ -1,3 +1,3 @@
 __appname__ = "raintale"
-__appversion__ = '0.2020.03.03.002430'
+__appversion__ = '0.2020.03.10.013046'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
