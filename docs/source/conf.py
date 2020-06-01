1a9aa359c32d61453ec452b3a65a79b8eab0e325
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-06-01 15:18:43 -0600

    raising version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2019.07.11.140435'
+release = u'0.2020.06.01.211830'
 
 
 # -- General configuration ---------------------------------------------------
--- raintale/version.py
+++ raintale/version.py
@@ -1,3 +1,3 @@
 __appname__ = "raintale"
-__appversion__ = '0.2019.07.11.140435'
+__appversion__ = '0.2020.06.01.211830'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
