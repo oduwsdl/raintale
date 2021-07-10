44dfb8042560246fafc082b974a175b942412c7b
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-07-10 11:54:22 -0600

    fixing installation script to only work in virtualenvs for now

--- raintale-gui/install-gui.sh
+++ raintale-gui/install-gui.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z $VIRTUAL_ENV ]; then
+    echo "It does not appear that you are installing Raintale's GUI inside a Python Virtualenv, quitting..."
+    exit 255
+fi
+
+echo "Installing GUI inside environment at $VIRTUAL_ENV"
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 if [ -z "$1" ]; then
@@ -8,13 +15,17 @@ else
     WOOEY_DIR=$1
 fi
 
-# pip install . --use-feature=in-tree-build
-# pip install Django==3.1.8 # see https://github.com/wooey/Wooey/issues/334
-# pip install wooey
+pip install . --use-feature=in-tree-build
+pip install Django==3.1.8 # see https://github.com/wooey/Wooey/issues/334
+pip install wooey
 
-# # create Wooey project
-# rm -rf ${WOOEY_DIR}
-# wooify -p ${WOOEY_DIR}
+# create Wooey project
+rm -rf ${WOOEY_DIR}
+
+echo "creating Wooey project"
+PROJECT_NAME=`basename ${WOOEY_DIR}`
+cd `dirname ${WOOEY_DIR}`
+wooify -p ${PROJECT_NAME}
 
 echo "changing to ${WOOEY_DIR}"
 cd ${WOOEY_DIR}
