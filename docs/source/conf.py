8ecdcbab128f40ce6d637795d6beb49be8ad0245
Shawn M. Jones <jones.shawn.m@gmail.com>
2021-11-14 20:51:24 -0700

    committing version 0.5.20211115035124 before taggin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Shawn M. Jones'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.5.20211115034029'
+release = u'0.5.20211115035124'
 
 # -- General configuration ---------------------------------------------------
 
--- raintale/version.py
+++ raintale/version.py
@@ -1,3 +1,3 @@
 __appname__ = "raintale"
-__appversion__ = '0.5.20211115034029'
+__appversion__ = '0.5.20211115035124'
 __useragent__ = "{}/{}".format(__appname__, __appversion__)
